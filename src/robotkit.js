e2ed2b4b2619ac267f40f6908c507798d426d1a7
Ranieri Junior <ran-junior@hotmail.com>
2021-04-14 19:38:58 -0300

    Add basic functions for robot

--- src/robotkit.js
+++ src/robotkit.js
@@ -18,10 +18,62 @@ class Rover {
     /**
      * Create a Rouver.
      */
-     constructor() {
+    constructor() {
         this.x = 0;
         this.y = 0;
         this.currentDirection = Directions.North;
         this.gridPoints = []
     }
+
+    /**
+     * Get Rouver direction and side
+     *
+     * @param {Sides} Side Determinate if rover should turn Right or Left
+     * @returns "X position Y position Direction"
+     * @type String
+     */
+    getCurrentLocation() {
+        return ""
+    }
+
+    /**
+     * Get Rouver grid
+     *
+     * @returns Rouver grid in [maxX, maxY]
+     * @type [number]
+     */
+    getCurrentGrid() {
+        return [];
+    }
+
+    /**
+     * Set Rouver position
+     *
+     * @param {number} x Rouver x position
+     * @param {number} y Rouver y position
+     * @param {Directions} direction Rouver direction
+     */
+    setPosition(x = 0, y = 0, direction) { }
+
+    /**
+     * Set Rouver grid
+     *
+     * @param {[number]} gridPoints Rouver grid [maxX, maxY] 
+     */
+    setGrid(gridPoints = []) { }
+
+    /**
+     * Move Rouver to specific point
+     *
+     * @param {string} commands Comands for Rouver execute like "MMRMMRMRRM"
+     * @param {[number]} gridPoints (OPTIONAL) Rouver grid [maxX, maxY] 
+     * @returns "X position Y position Direction"
+     * @type String
+     */
+    async move(commands, gridPoints = []) { }
+}
+
+module.exports = {
+    Directions,
+    Rover
 }
--- test/roverKit.test.js
+++ test/roverKit.test.js
@@ -1,8 +1,26 @@
-const { Directions, Rover } = require("../src/roverKit");
+const { Directions, Rover } = require("../src/robotkit");
 
 describe('Instance and methods test', () => {
     test('Should create a Rouver instance', () => {
         const rover = new Rover();
         expect(rover instanceof Rover).toBe(true);
+    })
+
+    test('Should check Rouver default position and set new Rouver position', () => {
+        const rover = new Rover();
+        const rouverStartPosition = rover.getCurrentLocation();
+        expect(rouverStartPosition).toBe("0 0 N");
+        rover.setPosition(3, 3, Directions.East);
+        const rouverNewPosition = rover.getCurrentLocation();
+        expect(rouverNewPosition).toBe("3 3 E");
+    })
+
+    test('Should check Rouver default grid and set new rouver grid', () => {
+        const rover = new Rover();
+        const rouverStartGrid = rover.getCurrentGrid();
+        expect(rouverStartGrid).toEqual([]);
+        rover.setGrid([5, 5]);
+        const rouverNewGrid = rover.getCurrentGrid();
+        expect(rouverNewGrid).toEqual([5, 5]);
     })
 })
